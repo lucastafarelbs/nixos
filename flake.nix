e5f3d5c04b881e84fc67420fdb2038e9a0149472
lucastafarelbs <lucastafarelbs@gmail.com>
2023-10-29 20:32:08 -0300

    enabling flakes

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  };
+ outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      thrive = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [./nixos/configuration.nix];
+      };
+    };
+
+  };
+}
