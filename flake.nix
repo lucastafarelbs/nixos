989bbc1fe70024959e793cfd7ddb9c40569fd8a7
lucastafarelbs <lucastafarelbs@gmail.com>
2023-10-29 21:26:51 -0300

    enabling home-manager

--- flake.nix
+++ flake.nix
@@ -2,9 +2,14 @@
   description = "NixOS Flake";
 
   inputs = {
+    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
- outputs = {
+  outputs = {
     self,
     nixpkgs,
     home-manager,
@@ -12,15 +17,20 @@
   } @ inputs: let
     inherit (self) outputs;
   in {
-    # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
       thrive = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
         modules = [./nixos/configuration.nix];
       };
     };
 
+    homeConfigurations = {
+      "qolab@thrive" = home-manager.lib.homeManagerConfiguration {
+	pkgs = nixpkgs.legacyPackages.x86_64-linux;
+	extraSpecialArgs = {inherit inputs outputs;};
+	modules = [./home-manager/home.nix];
+      };
+    };
   };
 }
